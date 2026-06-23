05a394ee30fbdbe3c96585996890554cfc24704a
Jonathan Duarte <devjonathanduarte@gmail.com>
2026-06-23 02:02:13 -0300

    bug fix: altera o nome das pastas do mocker

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,33 +1,20 @@
 from http import HTTPStatus
-from unittest.mock import patch
 
 import pytest
 
 from flask_blog_api.controllers.utils import requires_role
 
 
-@pytest.mark.parametrize(
-    "test_input, exc_class, msg",
-    [
-        (
-            "a",
-            TypeError,
-            "unsupported operand type(s) for ** or pow(): 'str' and 'int'",
-        ),
-        (
-            None,
-            TypeError,
-            "unsupported operand type(s) for ** or pow(): 'NoneType' and 'int'",
-        ),
-    ],
-)
 def test_requires_role_success(mocker):
 
     # Given
     mock_user = mocker.Mock()
     mock_user.role.name = "admin"
-    mocker.patch("controllers.utils.get_jwt_identity")
-    mocker.patch("controllers.utils.db.get_or_404", return_value=mock_user)
+    mocker.patch("flask_blog_api.controllers.utils.get_jwt_identity")
+    mocker.patch(
+        "flask_blog_api.controllers.utils.db.get_or_404",
+        return_value=mock_user,
+    )
     decorated_function = requires_role("admin")(lambda: "Success")
 
     # When
@@ -42,8 +29,11 @@ def test_requires_role_fail(mocker):
     # Given
     mock_user = mocker.Mock()
     mock_user.role.name = "normal"
-    mocker.patch("controllers.utils.get_jwt_identity")
-    mocker.patch("controllers.utils.db.get_or_404", return_value=mock_user)
+    mocker.patch("flask_blog_api.controllers.utils.get_jwt_identity")
+    mocker.patch(
+        "flask_blog_api.controllers.utils.db.get_or_404",
+        return_value=mock_user,
+    )
     decorated_function = requires_role("admin")(lambda: "Success")
 
     # When
